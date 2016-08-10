3f270f3b5240b64a1fbb69ebb7faaa913cc448b9
kay_yu <kay71029@gmail.com>
2016-08-10 02:43:13 +0000

    修改defense.php寫法

--- defense.php
+++ defense.php
@@ -2,9 +2,8 @@
     session_start();
     require("mysql.php");
     header('Content-Type: text/html; charset=utf-8');
-   if ( $_SESSION['ac_id']  != null) {
-   } else {
-     header("Refresh:0.5; url=login.php");
-     exit();
+    if ($_SESSION['ac_id'] = null) {
+    header("Refresh:0.5; url = login.php");
+    exit();
    }
 ?>
