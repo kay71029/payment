909168c116ae7aea38e3cc4752f5735b75aef05d
kay_yu <kay71029@gmail.com>
2016-08-11 02:34:38 +0000

    修改PayPost.php程式

--- PayPost.php
+++ PayPost.php
@@ -17,29 +17,30 @@
             $payMoney = $_POST["ac_acount"];
             $totalMoney = $orgMoney - $payMoney;
 
-            if ($totalMoney >= 0) {
-                $sql = "UPDATE `admin` SET `ac_acount`= `ac_acount` - :ac_acount WHERE `ac_id` = :ac_id ";
-                $result = $db->prepare($sql);
-                $result->bindParam(':ac_acount', $payMoney);
-                $result->bindParam(':ac_id', $_SESSION['ac_id']);
-                $result->execute();
-
-                $sql = "INSERT INTO `banker_detail`(`ac_id`, `type`, `money`, `date`, `blance`, `acountRecord`) VALUES (:ac_id, 2, :money, :date, :blance, :acountRecord)";
-                $result = $db->prepare($sql);
-                $result->bindParam(':ac_id', $_SESSION['ac_id']);
-                $result->bindParam(':money', $payMoney);
-                $result->bindParam(':date', $_POST["time"]);
-                $result->bindParam(':blance', $orgMoney);
-                $result->bindParam(':acountRecord', $totalMoney);
-                $result->execute();
-                $db->commit();
-            } else {
-                echo "餘額不足";
-                header("Refresh:0.5; url = PayMoney.php");
+            if ($orgMoney<$payMoney) {
+                throw new Exception("餘額不足");
             }
+            $sql = "UPDATE `admin` SET `ac_acount`= `ac_acount` - :ac_acount WHERE `ac_id` = :ac_id ";
+            $result = $db->prepare($sql);
+            $result->bindParam(':ac_acount', $payMoney);
+            $result->bindParam(':ac_id', $_SESSION['ac_id']);
+            $result->execute();
+
+            $sql = "INSERT INTO `banker_detail`(`ac_id`, `type`, `money`, `date`, `blance`, `acountRecord`) VALUES (:ac_id, 2, :money, :date, :blance, :acountRecord)";
+            $result = $db->prepare($sql);
+            $result->bindParam(':ac_id', $_SESSION['ac_id']);
+            $result->bindParam(':money', $payMoney);
+            $result->bindParam(':date', $_POST["time"]);
+            $result->bindParam(':blance', $orgMoney);
+            $result->bindParam(':acountRecord', $totalMoney);
+            $result->execute();
+            $db->commit();
+
         } catch (Exception $e) {
+             $db->rollBack();
             echo $e->getMessage();
-            $db->rollBack();
+            header("Refresh:0.5; url = PayMoney.php");
+            exit();
         }
         echo "新增成功";
         header("Refresh:0.5; url = AccountDetail.php");
