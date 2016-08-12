6c01750f95804b57fdfefba075b92175af09bae7
kay_yu <kay71029@gmail.com>
2016-08-12 05:45:41 +0000

    修改悲觀鎖格式

--- DoDeposit.php
+++ DoDeposit.php
@@ -16,13 +16,16 @@ if ($_POST["ac_acount"] != null) {
         $saveMoney = $_POST["ac_acount"];
         $totalMoney = $originalMoney + $saveMoney;
 
-        $sql = "UPDATE `admin` SET `ac_acount` = `ac_acount` + :ac_acount WHERE `ac_id` = :ac_id";
+        $sql = "UPDATE `admin` SET `ac_acount` = `ac_acount` + :ac_acount WHERE"
+        . "`ac_id` = :ac_id";
         $result = $db->prepare($sql);
         $result->bindParam(':ac_acount', $saveMoney);
         $result->bindParam(':ac_id', $_SESSION['ac_id']);
         $result->execute();
 
-        $sql = "INSERT INTO `banker_detail`(`ac_id`, `type`, `money`, `date`, `blance`, `newBlance`) VALUES (:ac_id, 1, :money, :date, :blance, :newBlance)";
+        $sql = "INSERT INTO `banker_detail`(`ac_id`, `type`, `money`, `date`, "
+            . "`blance`, `newBlance`) VALUES (:ac_id, 1, :money, :date, :blance, "
+            . ":newBlance)";
         $result = $db->prepare($sql);
         $result->bindParam(':ac_id', $_SESSION['ac_id']);
         $result->bindParam(':money', $saveMoney);
